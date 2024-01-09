f334c08fefb3d769161489ca551df199268c0de3
Joao Soares <jgsoares.francisco@gmail.com>
2024-01-09 00:45:50 -0300

    feat: get specific lead handler

--- handler/showLead.go
+++ handler/showLead.go
@@ -1,13 +1,27 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joaogabsoaresf/go-stock-watcher/schemas"
 )
 
 func ShowLeadHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Lead",
-	})
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	lead := schemas.Lead{}
+	// find lead
+	if err := db.First(&lead, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("lead with id: %s not found", id))
+		return
+	}
+
+	// return lead
+	sendSuccess(ctx, "find", lead)
 }
