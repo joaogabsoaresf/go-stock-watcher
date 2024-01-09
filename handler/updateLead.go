b634c37ed0a063380b505ff593603afcbf4b2745
Joao Soares <jgsoares.francisco@gmail.com>
2024-01-09 00:46:16 -0300

    feat: update lead handler

--- handler/updateLead.go
+++ handler/updateLead.go
@@ -1,13 +1,58 @@
 package handler
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joaogabsoaresf/go-stock-watcher/schemas"
 )
 
 func UpdateLeadHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "PUT Lead",
-	})
+	request := UpdateLeadRequest{}
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	id := ctx.Query("id")
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "queryParameter").Error())
+		return
+	}
+
+	lead := schemas.Lead{}
+
+	if err := db.First(&lead, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, fmt.Sprintf("lead with id: %s not found", id))
+		return
+	}
+
+	//update lead
+	if request.Name != "" {
+		lead.Name = request.Name
+	}
+	if request.Email != "" {
+		lead.Email = request.Email
+	}
+	if request.Phone != "" {
+		lead.Phone = request.Phone
+	}
+	if request.Client != nil {
+		lead.Client = *request.Client
+	}
+	if request.Age > 0 {
+		lead.Age = request.Age
+	}
+
+	// save lead
+	if err := db.Save(&lead).Error; err != nil {
+		logger.Errorf("error updating lead: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, fmt.Sprintf("erro updating lead id: %s", id))
+		return
+	}
+	sendSuccess(ctx, "update", lead)
 }
