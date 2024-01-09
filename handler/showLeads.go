b8807a02dd4d5b8fe6ce136a112b5b3023f6bbec
Joao Soares <jgsoares.francisco@gmail.com>
2024-01-09 00:46:01 -0300

    feat: get all leads handler

--- handler/showLeads.go
+++ handler/showLeads.go
@@ -4,10 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joaogabsoaresf/go-stock-watcher/schemas"
 )
 
 func ShowLeadsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Leads",
-	})
+	lead := []schemas.Lead{}
+
+	if err := db.Find(&lead).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error find lead")
+		return
+	}
+
+	sendSuccess(ctx, "list", lead)
 }
