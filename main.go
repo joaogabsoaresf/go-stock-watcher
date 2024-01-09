36ff1a54f019d03628331c392090794bfa3dfe93
Joao Soares <jgsoares.francisco@gmail.com>
2024-01-08 22:34:14 -0300

    feat: add logger

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/joaogabsoaresf/go-stock-watcher/router"
+import (
+	"github.com/joaogabsoaresf/go-stock-watcher/config"
+	"github.com/joaogabsoaresf/go-stock-watcher/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
-	// Inicializar o router
+	logger = *config.GetLogger("main")
+	// initialize config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("Config init error: %v", err)
+		return
+	}
+
+	// initialize router
 	router.Initialize()
 }
