541825531ce3844098deca8491c4b78474c47996
Joao Soares <jgsoares.francisco@gmail.com>
2024-01-08 23:51:10 -0300

    feat: add request and response handlers

---
+++ handler/request.go
@@ -0,0 +1,36 @@
+package handler
+
+import "fmt"
+
+func errParamIsRequired(name, typ string) error {
+	return fmt.Errorf("param: %s (type %s) is required", name, typ)
+}
+
+// create lead
+
+type CreateLeadRequest struct {
+	Name   string `json:"name"`
+	Email  string `json:"email"`
+	Phone  string `json:"phone"`
+	Client *bool  `json:"client"`
+	Age    int64  `json:"age"`
+}
+
+func (r *CreateLeadRequest) Validate() error {
+	if r.Name == "" {
+		return errParamIsRequired("name", "string")
+	}
+	if r.Email == "" {
+		return errParamIsRequired("email", "string")
+	}
+	if r.Phone == "" {
+		return errParamIsRequired("phone", "string")
+	}
+	if r.Client == nil {
+		return errParamIsRequired("client", "bool")
+	}
+	if r.Age <= 0 {
+		return errParamIsRequired("age", "int64")
+	}
+	return nil
+}
